c041b9378bbde977017f09e45ba2f63bf1267b69
AnildoFernando <anildofba@gmail.com>
2025-07-06 19:19:12 -0300

    Pesquisa pelo id e/ou name
    Pesquisa pelo id e/ou name trazendo o nome e imagem, botões de preview e next adicionados.

--- public/script.js
+++ public/script.js
@@ -1,44 +1,59 @@
-const listaTarefas = document.getElementById('listaTarefas');
-const form = document.getElementById('formTarefa');
-const input = document.getElementById('titulo');
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
 
-const api = 'http://localhost:3000/tarefas';
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
-async function carregarTarefas() {
-  listaTarefas.innerHTML = '';
-  const res = await fetch(api);
-  const tarefas = await res.json();
+let searchPokemon = 1;
 
-  tarefas.forEach(tarefa => {
-    const item = document.createElement('item');
-    item.textContent = tarefa.titulo;
-    if (tarefa.concluida) item.classList.add('completed');
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    const btn = document.createElement('button');
-    btn.textContent = 'X';
-    btn.onclick = async () => {
-      await fetch(`${api}/${tarefa.id}`, { method: 'DELETE' });
-      carregarTarefas();
-    };
-
-    item.appendChild(btn);
-    listaTarefas.appendChild(item);
-  });
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
-form.onsubmit = async (e) => {
-  e.preventDefault();
-  const titulo = input.value.trim();
-  if (!titulo) return;
+const renderPokemon = async (pokemon) => {
 
-  await fetch(api, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ titulo })
-  });
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
 
-  input.value = '';
-  carregarTarefas();
-};
+    const data = await fetchPokemon(pokemon);
 
-carregarTarefas();
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not found :(';
+        pokemonNumber.innerHTML = '';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+    }
+    renderPokemon(searchPokemon);
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
